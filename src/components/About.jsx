8f01930861157203dfa61b15b33318a124eab40b
falu27 <falu_cba@hotmail.com>
2023-07-04 19:41:19 -0300

    first commit portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-900 to-black text-white'>
+
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-600'>About</p>
+            </div>
+
+            <p className='text-xl mt-10'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis labore, suscipit, sed illo maxime non veritatis fuga adipisci soluta pariatur placeat necessitatibus libero! Consectetur voluptatum ratione ea ut aliquam at doloremque, consequatur labore quo nemo sunt? Distinctio ratione perferendis eos maiores, hic ipsam illum soluta reprehenderit ut rem eaque optio?
+            </p>
+
+            <br />
+
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur et quaerat nisi qui assumenda dolorum doloribus expedita ratione eos, amet, atque temporibus deleniti. Incidunt laudantium perferendis nesciunt eum reiciendis vitae itaque, non soluta eveniet? Ipsa ad magni, ullam soluta voluptates repellat porro natus excepturi culpa ipsam nemo maiores nulla tempora.
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
